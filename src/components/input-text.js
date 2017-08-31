f64ac86096fcc0cd96fb6a145feaab745fd28683
Ben Crisostomo <beni.blc@gmail.com>
2017-08-31 13:40:05 -0400

    added marked and cusomized files

---
+++ src/components/input-text.js
@@ -0,0 +1,7 @@
+import React, { Component} from 'react';
+
+class InputText extends Component {
+
+}
+
+export default ImportText;
---
+++ src/components/output-text.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+class OutputText extends Component {
+
+}
+
+export default OutputText;
